961fc9dd85f25c390dead8386a56f1056096e380
edu souza <edu.testes456@gmail.com>
2025-08-21 17:30:47 -0400

    adiciona pages ao projetos

---
+++ app/pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="bg-stone-100">
+    <SideBarMenu/>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+  const menuOpen = ref(false)
+
+
+</script>
+
+<style scoped>
+
+</style>
